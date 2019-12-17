565ac04de212eac9097780d63386ce3b964c6e76
Nico Manzanel <nicomanzanel@hotmail.com>
2019-12-17 14:50:14 -0300

    por ahora es el final

--- carrito.php
+++ carrito.php
@@ -89,7 +89,6 @@ $sHTML .= '<br> <hr> <br> <strong>Precio total:</strong> ' . $fPrecioTotal;
                
   <hr>
   <div>
-  
 		<?php echo $sHTML; ?>
 	</div>
 	
@@ -114,16 +113,125 @@ $sHTML .= '<br> <hr> <br> <strong>Precio total:</strong> ' . $fPrecioTotal;
         <li><a href="carrito.php?nombre=moto-G7&precio=18000">Motorola G7</a></li>
         -->
         <br>
-        <button type="button" class="btn btn-success">Comprar</button>
 		<button type="button" class="btn btn-danger"><a href="carrito.php?vaciar=1">Vaciar Carrito</a></button>
 	</ul>	  
  
   
 </div>
+<!-- Formulario Direccion del Cliente para ir al Pago con CreditCArd -->
+<form>
 
+    <div class="form-group-datos-cliente"> <!-- Full Name -->
+        <label for="full_name_id" class="control-label">Nombre completo</label>
+        <input type="text" class="form-control" id="full_name_id" name="full_name" placeholder="John Deer">
+    </div>    
+
+    <div class="form-group-datos-cliente"> <!-- Street 1 -->
+        <label for="street1_id" class="control-label">Dirreción</label>
+        <input type="text" class="form-control" id="street1_id" name="street1" placeholder="Nombre de la calle, Número, Piso, Depto">
+    </div>                       
+
+    <div class="form-group-datos-cliente"> <!-- City-->
+        <label for="city_id" class="control-label">Ciudad</label>
+        <input type="text" class="form-control" id="city_id" name="city" placeholder="Smallville">
+    </div>                                    
+                            
+    <div class="form-group-datos-cliente"> <!-- State Button -->
+        <label for="state_id" class="control-label">Provincia</label>
+        <select class="form-control" id="state_id">
+            <option value="SF">Santa Fe</option>
+            <option value="BS">Buenos Aires</option>
+            <option value="CB">Cordoba</option>
+            <option value="SC">Santa Cruz</option>
+            <option value="ER">Entre Rios</option>
+            <option value="CH">Chaco</option>
+            <option value="NE">Neuquén</option>
+            <option value="DE">Delaware</option>
+            <option value="DC">District Of Columbia</option>
+            <option value="FL">Florida</option>
+            <option value="GA">Georgia</option>
+            <option value="HI">Hawaii</option>
+            <option value="ID">Idaho</option>
+            <option value="IL">Illinois</option>
+            <option value="IN">Indiana</option>
+            <option value="IA">Iowa</option>
+            <option value="KS">Kansas</option>
+            <option value="KY">Kentucky</option>
+            <option value="LA">Louisiana</option>
+            <option value="ME">Maine</option>
+            <option value="MD">Maryland</option>
+            <option value="MA">Massachusetts</option>
+            <option value="MI">Michigan</option>
+            <option value="MN">Minnesota</option>
+            
+        </select>                    
+    </div>
+    
+    <div class="form-group-datos-cliente"> <!-- Zip Code-->
+        <label for="zip_id" class="control-label">Codigo postal</label>
+        <input type="text" class="form-control" id="zip_id" name="zip" placeholder="#####">
+    </div>        
+    <br><br>  
+    
+</form>
+<!-- Formulario Credit Card -->
+<form action="#" class="credit-card-div">
+<div class="panel panel-default" >
+ <div class="panel-heading">
+     
+      <div class="row ">
+              <div class="col-md-12">
+                  <input type="text" class="form-control" placeholder="Numero de la tarjeta de crédito" />
+              </div>
+          </div>
+     <div class="row ">
+              <div class="col-md-3 col-sm-3 col-xs-3">
+                  <span class="help-block text-muted small-font" >Mes de vencimiento</span>
+                  <input type="text" class="form-control" placeholder="MM" />
+              </div>
+         <div class="col-md-3 col-sm-3 col-xs-3">
+                  <span class="help-block text-muted small-font" >Año de vencimiento</span>
+                  <input type="text" class="form-control" placeholder="YY" />
+              </div>
+        <div class="col-md-3 col-sm-3 col-xs-3">
+                  <span class="help-block text-muted small-font" >  CCV</span>
+                  <input type="text" class="form-control" placeholder="CCV" />
+              </div>
+         <div class="col-md-3 col-sm-3 col-xs-3">
+<img src="img/credit-card-2.png" class="img-rounded" width="50px" />
+         </div>
+          </div>
+     <div class="row ">
+              <div class="col-md-12 pad-adjust">
+
+                  <input type="text" class="form-control" placeholder="Nombre de la tarjeta" />
+              </div>
+          </div>
+     <div class="row">
+<div class="col-md-12 pad-adjust">
+    <div class="checkbox">
+    <label>
+      <input type="checkbox" checked class="text-muted"> &nbspGuardar detalles para pagos rápidos? </a>
+    </label>
+  </div>
+</div>
+     </div>
+       <div class="row ">
+            <div class="col-md-6 col-sm-6 col-xs-6 pad-adjust">
+                 <input type="submit"  class="btn btn-danger" value="CANCELAR" />
+              </div>
+              <div class="col-md-6 col-sm-6 col-xs-6 pad-adjust">
+                  <input type="submit"  class="btn btn-warning btn-block" value="PAGAR AHORA" />
+              </div>
+          </div>
+     
+                   </div>
+              </div>
+</form>
 </section>
+
 <br><br><br><br><br><br><br><br>
-    <footer class="abajo">
+    <footer>
 <nav class="navbar navbar-expand-lg navbar-light ">
     <a href="index.php"><img src="img/LOGO.png" alt="logotipo" class="logo"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
